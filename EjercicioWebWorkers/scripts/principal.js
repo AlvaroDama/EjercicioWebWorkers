2ffe1348c2bb860eb7d2b967dda69a9b39a8b61b
Álvaro <alvaro.d.jimenez@gmail.com>
2015-10-06 14:27:43 +0200

    Revisar Date() y Math.Random. Terminar.

---
+++ EjercicioWebWorkers/scripts/principal.js
@@ -0,0 +1,14 @@
+﻿var reloj;
+
+var iniciaReloj = function() {
+
+    reloj = new Worker("scripts/reloj.js");
+
+    reloj.onmessage = function(e) {
+        document.getElementById("hora").innerHTML = e.data.toJSON();
+    };
+}
+
+iniciaReloj();
+
+//math.random --> hexadecimal (para el color aleatorio de cargar y cambiod e color)
---
+++ EjercicioWebWorkers/scripts/reloj.js
@@ -0,0 +1,6 @@
+﻿var reloj = new Date();
+
+function insular() {
+    reloj.setHours(reloj.getHours() + 1);
+    self.postMessage(reloj.toJSON());
+}
