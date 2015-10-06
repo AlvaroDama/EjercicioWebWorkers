0b174d6911ec4e4fbb546d9df2862b5286960d44
Álvaro Dama Jiménez <Alvaro.Dama@tajamar365.com>
2015-10-06 20:34:20 +0200

    Funcional sin colores

--- EjercicioWebWorkers/scripts/principal.js
+++ EjercicioWebWorkers/scripts/principal.js
@@ -1,14 +1,43 @@
 ﻿var reloj;
+var contador = 1;
 
 var iniciaReloj = function() {
 
     reloj = new Worker("scripts/reloj.js");
 
     reloj.onmessage = function(e) {
-        document.getElementById("hora").innerHTML = e.data.toJSON();
-    };
+        document.getElementById("hora").innerHTML = formateaHora(e.data.getHours());
+        document.getElementById("mins").innerHTML = formateaHora(e.data.getMinutes());
+        document.getElementById("secs").innerHTML = formateaHora(e.data.getSeconds());
+    }
+}
+
+function para() {
+    reloj.terminate();
+    reloj = undefined;
+}
+
+function reinicia() {
+    if (!reloj)
+        iniciaReloj();
+}
+
+function cambia() {
+    reloj.postMessage("insular");
+    contador += contador;
+    if (contador % 2 == 0)
+        document.getElementById("insular").innerHTML = "Insular";
+    else
+        document.getElementById("insular").innerHTML = "Peninsular";
 }
 
 iniciaReloj();
+document.getElementById("insular").addEventListener("click", cambia);
+document.getElementById("para").addEventListener("click", para);
+document.getElementById("inicia").addEventListener("click", reinicia);
 
+
+function formateaHora(dato) {
+    return dato<10?"0"+dato:dato;
+}
 //math.random --> hexadecimal (para el color aleatorio de cargar y cambiod e color)
--- EjercicioWebWorkers/scripts/reloj.js
+++ EjercicioWebWorkers/scripts/reloj.js
@@ -1,6 +1,24 @@
 ﻿var reloj = new Date();
+var boolPenin = undefined;
 
-function insular() {
-    reloj.setHours(reloj.getHours() + 1);
-    self.postMessage(reloj.toJSON());
+function mostrarHoras() {
+
+    reloj = new Date();
+
+    if (boolPenin)
+        reloj.setHours(reloj.getHours() - 1);
+    
+    self.postMessage(reloj);
+    setTimeout("mostrarHoras()", 1000);
 }
+
+self.onmessage = function (e) {
+    if (e.data == "insular") {
+        if (!boolPenin)
+            boolPenin = "true";
+        else
+            boolPenin = undefined;
+    }
+}
+
+mostrarHoras();
